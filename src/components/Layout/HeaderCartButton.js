8d9dd6c500d2a2a6daf9bbad80e7ac9a1e776b07
berkagmpp <berkagmpp@gmail.com>
2022-04-18 22:32:04 +1200

    add dummy-meals / Meals / MealsSummary in Meals

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,7 +6,7 @@ import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = props => {
     return (
         <button type="button" className={classes.button}>
-            <span><CartIcon /></span>
+            <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>3</span>
         </button>
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import MealsSummary from "./MealsSummary";
+
+const Meals = props => {
+    return (
+        <Fragment>
+            <MealsSummary />
+        </Fragment>
+    )
+}
+
+export default Meals;
