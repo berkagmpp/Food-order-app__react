f5320da0dd1011f98603227b558c968c92b49ed6
berkagmpp <berkagmpp@gmail.com>
2022-04-21 14:30:31 +1200

    useRef() for passing Input amount of form (MealItemForm.js) / add React.forwardRef() to Input.js(customed component)

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;     // current.value is always string!
+        const enteredAmountNumber = +enteredAmount;     // convert string to number  
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount"
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input label="Amount" 
+                   ref={amountInputRef}
                    input= {{
                         id: 'amount_' + props.id,
                         type: 'number',
@@ -15,6 +34,7 @@ const MealItemForm = props => {
                    }}
             />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 }
