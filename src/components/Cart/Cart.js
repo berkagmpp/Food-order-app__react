51781f1868c9dc542b655eaa83c4d639c3c8af69
berkagmpp <berkagmpp@gmail.com>
2022-04-25 13:50:03 +1200

    import CartItem as a component and pass item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
-import react, { useContext } from 'react';
+import eact, { useContext } from 'react';
 
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
     const cartCxt = useContext(CartContext);
@@ -10,10 +11,18 @@ const Cart = props => {
     const totalAmount = `$${cartCxt.totalAmount.toFixed(2)}`;
     const hasItems = cartCxt.items.length > 0;
 
+    const cartItemRemoveHandler = id => {};
+    const cartItemAddHandler = item => {};
+
     const cartItems = <ul className={classes['cart-items']}> {
-        cartCxt.items.map(
-            item => <li key={item.id}>{item.name}</li>
-        )
+        cartCxt.items.map((item) => (
+            <CartItem key={item.id} 
+                      name={item.name}
+                      amount={item.amount}
+                      price={item.price} 
+                      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                      onAdd={cartItemAddHandler.bind(null, item)} />
+        ))
     }</ul>
 
     return (
